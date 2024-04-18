59bdd4dc3d58e84f1c919cb73ec6ff2d320a5ead
Nalin-kumar-gupta <nalingupta71@gmail.com>
2024-04-18 16:21:41 +0530

    amdfas init

---
+++ amdfas/settings/file_analyzer_settings.py
@@ -0,0 +1,8 @@
+from .base import *
+
+INSTALLED_APPS += [
+    # project1-specific apps
+]
+MIDDLEWARE += [
+    # project1-specific middleware
+]
---
+++ amdfas/settings/memory_analyzer_settings.py
@@ -0,0 +1,9 @@
+from .base import *
+
+
+INSTALLED_APPS += [
+    # project1-specific apps
+]
+MIDDLEWARE += [
+    # project1-specific middleware
+]
